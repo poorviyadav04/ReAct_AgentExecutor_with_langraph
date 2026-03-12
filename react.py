1a851f11e6ad6fdeaa49a13221ab7a944f0ce12d
Poorvi Yadav <poorviyadav008@gmail.com>
2026-03-12 13:10:18 +0530

    Function calling reasoning

---
+++ react.py
@@ -0,0 +1,20 @@
+from dotenv import load_dotenv
+from langchain_core.tools import tool
+from langchain_anthropic import ChatAnthropic
+from langchain_tavily import TavilySearch
+
+load_dotenv()
+
+@tool
+def triple(num:float) -> float:
+    """
+    param num: a number to triple
+    returns: the triple of the input number
+    """
+    return float(num) * 3
+
+tools = [TavilySearch(max_results = 1), triple]
+
+llm = ChatAnthropic(
+    model="claude-3-5-sonnet-latest",
+    temperature=0).bind_tools(tools)
